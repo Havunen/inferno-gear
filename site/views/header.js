dbc66f02c80a8f84fc1379ad42ca9a921cc97b18
Sampo Kivistö <sampo.kivisto@live.fi>
2016-01-22 15:32:56 +0200

    add favicon + filtering text dropdown items

--- site/views/header.js
+++ site/views/header.js
@@ -13,10 +13,10 @@ export class Header extends Component {
                 <h1><span>i</span>gear</h1>
                 <p>Open source component library for <a href="https://github.com/trueadm/inferno">InfernoJS</a>. Inspired by <a href="https://www.google.com/design/spec/material-design/introduction.html">Google material design</a> and in place editing.<br />
                 In my opinion runtime performance and instant responsivness of UI controls is really important for web applications, which is why I started building this library.<br />
-                To provide basic web controls with superior performance and simple design.<br />
-
+                To provide basic web controls with superior performance and simple design.
+                <br />
                 Inferno iGear is currently under active development, I'm adding more features each day.
-
+                <br />
                 Special thanks for <a href="https://github.com/trueadm">@trueadm</a>, <a href="https://github.com/kflash">@kflash</a>, <a href="https://github.com/kuraga">@kuraga</a>, <a href="https://github.com/tejacques">@tejacques</a> and rest of <a href="https://github.com/trueadm/inferno/graphs/contributors">InfernoJS contributors</a> who has made this library possible!</p>
                 <h3>
                     <span class="links note">v{packageJSON.version}</span>
