d756eba4ea1292a451f64cdc173e25c97bb0aae4
Sampo Kivistö <sampo.kivisto@live.fi>
2016-01-24 21:45:10 +0200

    Added prevMonth / nextMonth functions to calendar

--- site/site.js
+++ site/site.js
@@ -4,3 +4,37 @@ import {Page} from './views/page';
 
 InfernoDOM.render(<Header />, document.getElementById('header'));
 InfernoDOM.render(<Page />, document.getElementById('content'));
+
+//
+//class ChangeChildrenCount extends Component {
+//    constructor(props) {
+//        super(props);
+//
+//        this.state = {
+//            list: ['1', '2', '3', '4']
+//        };
+//
+//        // Bindings
+//        this.handleClick = this.handleClick.bind(this);
+//    }
+//
+//    handleClick() {
+//        this.setState({
+//            list: ['1']
+//        });
+//    }
+//
+//    render() {
+//        return (
+//            <div>
+//                <button onClick={this.handleClick}>1</button>
+//                {this.state.list.map(function (x, i) {
+//                    return <div>{i}</div>
+//                })}
+//            </div>
+//        );
+//    }
+//}
+//
+//
+//InfernoDOM.render(<ChangeChildrenCount />, document.body);
