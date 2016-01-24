a7210e5545b36cb67eea5d4e0474fe499887282b
Sampo Kivistö <sampo.kivisto@live.fi>
2016-01-24 17:59:11 +0200

    Working on calendar view

--- site/site.js
+++ site/site.js
@@ -4,22 +4,3 @@ import {Page} from './views/page';
 
 InfernoDOM.render(<Header />, document.getElementById('header'));
 InfernoDOM.render(<Page />, document.getElementById('content'));
-
-
-
-//class StyleIssue extends Component {
-//    constructor(props) {
-//        super(props);
-//    }
-//
-//    render() {
-//        return (
-//            <div>
-//                <div style={{'display': 'block'}}>Hi, I'm red</div>
-//            </div>
-//        )
-//    }
-//}
-
-
-//InfernoDOM.render(<StyleIssue />, document.body);
