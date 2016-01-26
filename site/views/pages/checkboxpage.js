28b2d430e7c4aa54bf8d52ccede240164c80ac4f
Sampo Kivistö <sampo.kivisto@live.fi>
2016-01-26 20:11:34 +0200

    text

--- site/views/pages/checkboxpage.js
+++ site/views/pages/checkboxpage.js
@@ -13,8 +13,7 @@ export class CheckboxPage extends Component {
                 <h2 class="separator"><span class="h2text">Checkbox</span></h2>
 
                 <div class="code-example">
-                    <span class="description">Avatar is simple stateless component that will display user picture if its available. <br />
-                        If picture is not available it displays first characters of up to two first letters. Background colour is calulated based on title.</span>
+                    <span class="description">checkbox</span>
                     <div class="example">
                         <IGearCheckbox checked={true} text="JabaDii" />
                         <IGearCheckbox text="" />
